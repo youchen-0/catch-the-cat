90bb6e6307f49e05ccbfb3ce0a6bc310afeea947
zhangyouchen <67458623+zhangyouchen@users.noreply.github.com>
2021-07-21 01:33:13 +0000

    Create run.js

---
+++ run.js
@@ -0,0 +1,9 @@
+window.game = new CatchTheCatGame({
+        w: 11,
+        h: 11,
+        r: 20,
+        backgroundColor: '#ffffff',
+        parent: 'catch-the-cat',
+        statusBarAlign: 'center',
+        credit: ''
+    });
